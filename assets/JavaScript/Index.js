0d40cd3fd8d3cdc49fa6281280e3a4de6d97af2f
lovapota <rfenosoa1@gmail.com>
2022-04-19 16:54:27 -0700

    agrandir les police de caractere

--- assets/JavaScript/Index.js
+++ assets/JavaScript/Index.js
@@ -1,8 +1,9 @@
 //alerte pour le boutton
 let myBtn = document.getElementById("lova")
 
-myBtn.addEventListener("click",iza)
-
 function iza(){
     alert("Tsy madeha io button io !")
 }
+
+myBtn.addEventListener("click",iza)
+
